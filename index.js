1ef1ad0da704d0e36d13e27a2a732444c3ac5fc2
Shreehari N W <34746409+shreeharinw@users.noreply.github.com>
2022-09-18 18:27:50 +0530

    Added source code and resources

---
+++ index.js
@@ -0,0 +1,646 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.5,
+    price: 13,
+    img: 'images/biryani/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/biryani/egg-biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/goan-fish-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'images/biryani/hyd-Mutton-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'images/biryani/kamrupi-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/biryani/kashmiri.pulao.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'images/biryani/memonibiryani.png',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/mughlai-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'images/chicken/Chicken_roast.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chicken/Chicken-Curry.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'images/chicken/Chicken-do-Pyaza.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chicken/Chicken-Masala.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'images/chicken/Handi-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Murgh Musallam',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'images/chicken/Murgh-Musallam.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/paneer/Matar-Paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'images/paneer/palak-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/paneer/paneer-butter-masala.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: 'images/paneer/Paneer-Do-Pyaza.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: 'images/paneer/Paneer-Hyderabadi.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 4.3,
+    price: 7,
+    img: 'images/paneer/paneer-lababdar.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 5,
+    img: 'images/paneer/shahi-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'images/vegetable/navratan-korma_-vegetable.png',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/vegetable/VEG-JALFREZI.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'images/vegetable/vegetable-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/vegetable/vegetable-curry.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Veg Kolhapur',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 10,
+    img: 'images/vegetable/vegetable-kolhapuri.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/vegetable/vegetable-masala.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/vegetable/vegetable-pakora.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/cabbage-momos-.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'images/chinese/ChickenManchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'images/chinese/Chili-Chicken.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 16,
+    img: 'images/chinese/chowmin.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 14,
+    img: 'images/chinese/spring-rolls.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chinese/szechuan-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/veg-fried-rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 18,
+    img: 'images/south indian/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 20,
+    img: 'images/south indian/idli-with-rice-flour.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 12,
+    img: 'images/south indian/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 10,
+    img: 'images/south indian/mysore-bonda.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 15,
+    img: 'images/south indian/onion-uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 40,
+    img: 'images/south indian/plain-dosa.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 25,
+    img: 'images/south indian/Rava-Uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south-indian',
+    rating : 4.3,
+    price: 34,
+    img: 'images/south indian/sambhar-vada.jpg',
+    quantity: 1
+},
+]
+
+
+function displayItems(){
+
+    let categories = ['biryani','chicken','paneer','vegetable','chinese','south-indian'];
+
+    categories.forEach(category=>{
+        let categoryElement = document.getElementById(category);
+        let categoryData = foodItem.filter(item=>item.category===category);
+        categoryData.forEach(item=>{
+            let itemCard = prepareItemCard(item);
+            categoryElement.appendChild(itemCard);
+        })
+    })
+    function prepareItemCard(item) {
+        let itemCard = document.createElement("div");
+        itemCard.setAttribute('id', 'item-card');
+
+        let cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        let star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = item.rating;
+
+        let heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        let img = document.createElement('img');
+        img.src = item.img;
+
+        let itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        let itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = `Price : $ ${item.price}`; //'Price : $' + item.price
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+        return itemCard;
+    }
+}
+
+// a = 10 elements 10 times separtely and 1 time you prcoess 
+
+// memory and time...100 times (lot of time)...... u can load the data in ur db in memory and 
+// verify the same in memory 
+
+
+function quickSearch(){
+    let categoryList = document.getElementById('category-list');
+    console.log(foodItem.map(item=>[item['category'],item]));
+    console.log(new Map(foodItem.map(item=>[item['category'],item])));
+    let quickSearchOptions = [...new Map(foodItem.map(item=>[item['category'],item])).values()];
+    quickSearchOptions.forEach(item=>{
+        console.log(item);
+        let listCard= document.createElement('div');
+        listCard.setAttribute('class','list-card');
+
+        let listImg = document.createElement('img')
+        listImg.src = item.img;
+
+        let listName = document.createElement('a');
+        listName.setAttribute('class','list-name');
+        listName.innerText = item.category;
+        listName.setAttribute('href','#'+item.category);
+
+        listCard.appendChild(listImg);
+        listCard.appendChild(listName);
+        categoryList.appendChild(listCard);
+    })
+}
+
+
+displayItems();
+quickSearch();
+
+window.onload = function(){
+    addEvents();
+}
+
+let cardData = []
+function addToCart(){
+    let itemToAdd = this.id;
+    console.log("this obj",this);
+    console.log('id',itemToAdd);
+    let itemObj = foodItem.find(item=>item.id==itemToAdd);
+    let itemIndexInCart = cardData.indexOf(itemObj);
+    if(itemIndexInCart === -1){
+        cardData = [...cardData,itemObj];
+        document.getElementById(itemToAdd).classList.add('toggle-heart');
+    }else{
+        alert("added to card already");
+    }
+    document.getElementById('cart-plus').innerText = ' ' +
+    cardData.length + ' Items';
+    totalAmount();
+    cartItems();
+}
+
+function cartItems(){
+    let tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    cardData.map(item=>{
+        let tableRow = document.createElement('tr');
+        let imageData = document.createElement('td');
+        let img = document.createElement('img');
+        img.src = item.img;
+        imageData.appendChild(img);
+
+        let itemNameData = document.createElement('td');
+        itemNameData.innerText = item.name;
+
+        let quantityData = prepareQuantityData(item);
+
+        let priceData = document.createElement('td');
+        priceData.innerText = item.price;
+
+        tableRow.appendChild(imageData);
+        tableRow.appendChild(itemNameData);
+        tableRow.appendChild(quantityData);
+        tableRow.appendChild(priceData);
+
+        tableBody.appendChild(tableRow);
+    })
+
+    document.querySelectorAll('.increase-item').forEach(item=>item.addEventListener('click',incrementItem));
+    document.querySelectorAll('.decrease-item').forEach(item=>item.addEventListener('click',decrementItem));
+
+    function prepareQuantityData(item) {
+        let quantityData = document.createElement('td');
+        let decrementButton = document.createElement('button');
+        decrementButton.setAttribute('class', 'decrease-item');
+        decrementButton.innerText = '-';
+        let quantityInfo = document.createElement('span');
+        quantityInfo.innerText = item.quantity;
+        let incrementButton = document.createElement('button');
+        incrementButton.setAttribute('class', 'increase-item');
+        incrementButton.innerText = '+';
+
+        quantityData.appendChild(decrementButton);
+        quantityData.appendChild(quantityInfo);
+        quantityData.appendChild(incrementButton);
+        return quantityData;
+    }
+}
+
+function incrementItem(){
+    console.log("Item to be incremented",this);
+    console.log("Item to be decremented",this);
+    let itemToIncrement = this.parentNode.previousSibling.innerText;
+    console.log("item name to be decremented",itemToIncrement);
+    let incrementObj = cardData.find(item=>item.name==itemToIncrement);
+    incrementObj.quantity+=1;
+    cartItems();
+    totalAmount();
+}
+
+function decrementItem(){
+    console.log("Item to be decremented",this);
+    let itemToDecrment = this.parentNode.previousSibling.innerText;
+    console.log("item name to be decremented",itemToDecrment);
+    let decrementObj = cardData.find(item=>item.name==itemToDecrment);
+    if(decrementObj.quantity>1){
+        decrementObj.quantity-=1;
+    }else{
+        let index = cardData.indexOf(decrementObj);
+        document.getElementById(decrementObj.id).classList.remove('toggle-heart');
+        cardData.splice(index,1);
+        document.getElementById('cart-plus').innerText = ' ' + cardData.length + ' Items';
+        if(cardData.length<1){
+            document.getElementById('food-items').classList.toggle('food-items');
+            document.getElementById('category-list').classList.toggle('food-items');
+            document.getElementById('category-header').classList.toggle('toggle-category');
+            document.getElementById('cart-page').classList.toggle('cart-toggle');
+            document.getElementById('checkout').classList.toggle('cart-toggle');
+        }
+    }
+    cartItems();
+    totalAmount();
+}
+
+function totalAmount(){
+    let totalQuantity = cardData.reduce((acc,item)=>{
+        return acc+item.quantity;
+    },0);
+    let totalPrice = cardData.reduce((acc,item)=>{
+        return acc+(item.quantity*item.price);
+    },0);
+    document.getElementById('total-item').innerText = 'Total Items: ' + totalQuantity;
+    document.getElementById('total-price').innerText = 'Total Price: ' + totalPrice;
+}
+
+
+function cartToggle(){
+    if(cardData.length>0){
+        document.getElementById('food-items').classList.toggle('food-items');
+        document.getElementById('category-list').classList.toggle('food-items');
+        document.getElementById('category-header').classList.toggle('toggle-category');
+        document.getElementById('cart-page').classList.toggle('cart-toggle');
+        document.getElementById('checkout').classList.toggle('cart-toggle');
+    } else{
+        alert("Currently there are no items in the cart");
+    }
+}
+
+function addEvents(){
+    document.querySelectorAll('.add-to-cart')
+    .forEach(item=>item.addEventListener('click',addToCart));
+
+    document.getElementById('cart-plus').addEventListener('click',cartToggle);
+
+    document.querySelectorAll('.cart-btn').forEach(item=>{
+        item.addEventListener('click',()=>{
+            cardData=[];
+            alert("Order placed successfully");
+            window.location.reload();
+        })
+    })
+
+    document.getElementById('add-address').addEventListener('click',addAddress);
+
+}
+
+function addAddress(){
+    let address  = prompt("Enter your address");
+    if(address){
+        document.getElementById('add-address').innerText=` ${address}`;
+    }else{
+        alert("Invalid address");
+    }
+}
+
